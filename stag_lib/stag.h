3bd9cdd383e6ce5acd1fe7eef18357309e30ff7c
Peter Macgregor <macgregor.pr@gmail.com>
2022-10-10 10:25:40 +0100

    Prepare release 0.1.6

---
+++ stag_lib/stag.h
@@ -0,0 +1,12 @@
+#ifndef STAG_TEST_STAG_H
+#define STAG_TEST_STAG_H
+
+namespace stag {
+  // Provide metadata about the library version
+  static const unsigned VERSION_MAJOR = 0;
+  static const unsigned VERSION_MINOR = 1;
+  static const unsigned VERSION_PATCH = 6;
+
+}
+
+#endif //STAG_TEST_STAG_H
