e0c070759e48d9b252a84d33debdaae461f62582
Peter Macgregor <peter.macgregor@ed.ac.uk>
2023-02-10 15:55:23 +0000

    Tidy up random.h documentation

--- stag_lib/random.h
+++ stag_lib/random.h
@@ -16,7 +16,7 @@
 
 namespace stag {
   /**
-   * Generate a graph from the symmetric stochastic block model.
+   * \brief Generate a graph from the symmetric stochastic block model.
    *
    * Every cluster has the same number of vertices. For large enough values of
    * n, this method samples from an approximate stochastic block model by
@@ -24,16 +24,18 @@ namespace stag {
    * exactly from the stochastic block model, pass the optional 'exact'
    * parameter to the method.
    *
-   * The approximate sampling method has running time O(k^2 + nnz) and the exact
-   * method has running time O(n^2) where nnz is the number of non-zeros in the
-   * generated graph's adjacency matrix.
+   * The approximate sampling method has running time \f$O(k^2 + \mathrm{nnz})\f$
+   * where \f$\mathrm{nnz}\f$ is the number of non-zeros in the generated
+   * graph's adjacency matrix,
+   * and the exact
+   * method has running time \f$O(n^2)\f$.
    *
-   * @param n - the number of vertices in the graph
-   * @param k - the number of clusters; vertices are split evenly between clusters
-   * @param p - the probability of including an edge inside a cluster
-   * @param q - the probability of including an edge between two clusters
-   * @param exact (optional) - whether to use the exact probability distribution
-   *                           or an approximation.
+   * @param n the number of vertices in the graph
+   * @param k the number of clusters; vertices are split evenly between clusters
+   * @param p the probability of including each edge inside a cluster
+   * @param q the probability of including each edge between two clusters
+   * @param exact (optional) whether to use the exact probability distribution
+   *                           or an approximation
    * @return the randomly generated graph
    */
   Graph sbm(stag_int n, stag_int k, double p, double q, bool exact);
@@ -44,18 +46,18 @@ namespace stag {
   Graph sbm(stag_int n, stag_int k, double p, double q);
 
   /**
-   * Generate a graph from the Erdos-Renyi model.
+   * \brief Generate a graph from the Erdos-Renyi model.
    *
    * For large values of n, this method will use an approximate version of the
-   * random model with running time O(nnz) where nnz is the number of edges in
-   * the sampled graph.
+   * random model with running time \f$O(\mathrm{nnz})\f$ where
+   * \f$\mathrm{nnz}\f$ is the number of edges in the generated graph.
    *
    * If the 'exact' parameter is true, then the true Erdos-Renyi distribution
-   * will be used, with running time O(n^2).
+   * will be used, with running time \f$O(n^2)\f$.
    *
-   * @param n - the number of vertices in the graph
-   * @param p - the probability of including each edge
-   * @param exact (optional) - whether to sample from the exact model
+   * @param n the number of vertices in the graph
+   * @param p the probability of including each edge
+   * @param exact (optional) whether to sample from the exact model
    * @return the randomly generated graph
    */
   Graph erdos_renyi(stag_int n, double p, bool exact);
