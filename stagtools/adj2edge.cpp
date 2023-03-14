33df5b9e058daf76e37106383e45cfa9f2066514
Peter Macgregor <peter.macgregor@ed.ac.uk>
2023-03-14 10:39:38 +0000

    Update stag tool usage messages

--- stagtools/adj2edge.cpp
+++ stagtools/adj2edge.cpp
@@ -6,17 +6,35 @@
 #include <cerrno>
 #include "graphio.h"
 
+void print_usage() {
+  std::cout << "Usage: stag_adj2edge [adjacencylist] [edgelist]" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Convert a STAG adjacency list file into an edgelist file." << std::endl;
+  std::cout << std::endl;
+  std::cout << "Arguments:" << std::endl;
+  std::cout << "  [adjacencylist]   the name of the adjacencylist file to be converted" << std::endl;
+  std::cout << "  [edgelist]        the name of the new edgelist file to be written" << std::endl;
+}
+
+
 int main(int argc, char** args) {
   // This program takes two arguments: the edgelist file and the adjacencylist
   // file to write to.
   if (argc != 3) {
-    std::cout << "This program expects 2 command line arguments." << std::endl;
+    print_usage();
     return EINVAL;
   }
 
   // Extract the command lien arguments.
-  std::string adj_fname(args[1]);
-  std::string edge_fname(args[2]);
+  std::string adj_fname;
+  std::string edge_fname;
+  try {
+    adj_fname = std::string(args[1]);
+    edge_fname = std::string(args[2]);
+  } catch (...) {
+    print_usage();
+    return EINVAL;
+  }
 
   stag::adjacencylist_to_edgelist(adj_fname, edge_fname);
 
