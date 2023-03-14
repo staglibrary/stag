d1b2b7632835d9b8fbd8ecbe62f965d94e622acf
Peter Macgregor <peter.macgregor@ed.ac.uk>
2023-03-14 10:30:16 +0000

    Update usage for stag_sbm

--- stagtools/sbm.cpp
+++ stagtools/sbm.cpp
@@ -7,6 +7,19 @@
 #include "graphio.h"
 #include "random.h"
 
+void print_usage() {
+  std::cout << "Usage: stag_sbm [filename] [n] [k] [p] [q]" << std::endl;
+  std::cout << std::endl;
+  std::cout << "This program generates a graph from the symmetric stochastic block model." << std::endl;
+  std::cout << std::endl;
+  std::cout << "Arguments:" << std::endl;
+  std::cout << "  [filename]        the name of the edgelist file to be written" << std::endl;
+  std::cout << "  [n]               the number of vertices in the graph" << std::endl;
+  std::cout << "  [k]               the number of clusters in the graph" << std::endl;
+  std::cout << "  [p]               each edge inside a cluster is included with probability p" << std::endl;
+  std::cout << "  [q]               each edge between clusters is included with probability q" << std::endl;
+}
+
 int main(int argc, char** args) {
   // This program takes 5 arguments:
   //   - the name of the edgelist file to generate
@@ -15,23 +28,24 @@ int main(int argc, char** args) {
   //   - the probability of each internal edge
   //   - the probability of each external edge
   if (argc != 6) {
-    std::cout << "This program expects 5 command line arguments:" << std::endl;
-    std::cout << "  - the name of the edgelist file to write the graph to" << std::endl;
-    std::cout << "  - the number of vertices in the graph" << std::endl;
-    std::cout << "  - the number of clusters in the graph" << std::endl;
-    std::cout << "  - the probability of each internal edge" << std::endl;
-    std::cout << "  - the probability of each external edge" << std::endl;
-    std::cout << "For example:" << std::endl;
-    std::cout << "    stag_sbm mygraph.edgelist 1000 5 0.3 0.01" << std::endl;
+    print_usage();
     return EINVAL;
   }
 
   // Extract the command line arguments.
-  std::string edge_fname(args[1]);
-  stag_int n = std::stoi(args[2]);
-  stag_int k = std::stoi(args[3]);
-  double p = std::stod(args[4]);
-  double q = std::stod(args[5]);
+  std::string edge_fname;
+  stag_int n, k;
+  double p, q;
+  try {
+    edge_fname = std::string(args[1]);
+    n = std::stoi(args[2]);
+    k = std::stoi(args[3]);
+    p = std::stod(args[4]);
+    q = std::stod(args[5]);
+  } catch (...) {
+    print_usage();
+    return EINVAL;
+  }
 
   // Prepare the general SBM arguments
   stag_int cluster_size = floor(((double) n) / ((double) k));
