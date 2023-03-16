ba80fd46c2e05fddbf2177d2411ac4ce998919c2
Peter Macgregor <peter.macgregor@ed.ac.uk>
2023-03-16 16:23:28 +0000

    Update docs for graphio with adjacency list

--- stag_lib/graphio.h
+++ stag_lib/graphio.h
@@ -6,6 +6,9 @@
 /**
  * @file graphio.h
  * \brief Methods for reading and writing graphs to disk.
+ *
+ * For an introduction to the file formats supported by STAG, see
+ * [Graph File Formats](@ref file-formats).
  */
 
 #ifndef STAG_TEST_GRAPHIO_H
@@ -81,10 +84,40 @@ namespace stag {
   /**
    * Load a graph from an adjacencylist file.
    *
-   * This method loads the entire graph into memory.
+   * The adjacency list file format is defined in the following way.
+   *   - Any lines beginning with `#` or `//` are ignored
+   *   - Any blank lines are ignored
+   *   - All other lines have the format `<node_id>: <list of neighbours>`, where
+   *     `<node_id>` is an integer and `<list of neighbours>` is either a
+   *     space-separated list of integers or a space-separated list of
+   *     `<id>:<weight>` where `<id>` gives the id of the neighbour and
+   *     `<weight>` is the weight of the edge.
+   *   - The `<node IDs>` of each line must be sorted in increasing order.
+   *   - The graph should have no self-loops.
    *
-   * @param filename
+   * Here is an example adjacencylist file.
+   *
+   *     # This line is ignored
+   *     0: 1 2
+   *     1: 0 2 3
+   *     2: 0 1
+   *     3: 1
+   *
+   * The following example includes weighted edges.
+   *
+   *     # This line is ignored
+   *     0: 1:0.5 2:0.5
+   *     1: 0:0.5 2:1
+   *     2: 0:0.5 1:1
+   *
+   * Note that this method loads the entire graph into memory. For large graphs,
+   * the stag::AdjacencyListLocalGraph object can be used to access the graph
+   * in a 'local' way without reading the entire graph into memory.
+   *
+   * @param filename the name of the adjacency list file to be loaded
    * @return stag::Graph object
+   * @throws std::runtime_error if the file doesn't exist or cannot be parsed as
+   *         an adjacency list
    */
   stag::Graph load_adjacencylist(std::string& filename);
 
@@ -97,7 +130,7 @@ namespace stag {
   void save_adjacencylist(stag::Graph &graph, std::string& filename);
 
   /**
-   * Convert an edgelist in the specified file to an adjacencylist.
+   * Convert an edgelist file to an adjacency list.
    *
    * @param edgelist_fname the name of the file containing the edgelist.
    * @param adjacencylist_fname the name of the file to write the adjacencylist.
@@ -106,9 +139,9 @@ namespace stag {
                                  std::string& adjacencylist_fname);
 
   /**
-   * Convert an adjacencylist in the specified file to an edgelist.
+   * Convert an adjacency list file to an edgelist.
    *
-   * @param adjacencylist_fname the name of the file containing the adjacencylist.
+   * @param adjacencylist_fname the name of the file containing the adjacency list.
    * @param edgelist_fname the name of the file to write the edgelist.
    */
   void adjacencylist_to_edgelist(std::string& adjacencylist_fname,
